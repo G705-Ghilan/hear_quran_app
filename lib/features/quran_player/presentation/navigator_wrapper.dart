3853fbabd045999e1430381c720d43304ad0007c
Ghilan Al-harazi <78649813+G705-Ghilan@users.noreply.github.com>
2023-10-17 12:37:21 +0300

    v1.0.3+6: back navigation feature

--- lib/features/quran_player/presentation/navigator_wrapper.dart
+++ lib/features/quran_player/presentation/navigator_wrapper.dart
@@ -9,52 +9,63 @@ import 'package:miniplayer/miniplayer.dart';
 class NavigatorWrapper extends StatelessWidget {
   const NavigatorWrapper({super.key, required this.navigationShell});
   final StatefulNavigationShell navigationShell;
+  static final List<int> _tabsHistory = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<QuranPlayerCubit, QuranPlayerState>(
-        builder: (context, state) {
-          return Stack(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: state.miniPlayer ? 70 : 0),
-                child: navigationShell,
-              ),
-              if (state.miniPlayer) const MiniPlayerWidget(),
-            ],
-          );
-        },
-      ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: onDestinationSelected,
-        selectedIndex: navigationShell.currentIndex,
-        destinations: [
-          NavigationDestination(
-            icon: const Icon(Icons.home_outlined),
-            selectedIcon: const Icon(Icons.home_filled),
-            label: context.lang.home,
-            tooltip: context.lang.home,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.favorite_border_rounded),
-            selectedIcon: const Icon(Icons.favorite),
-            label: context.lang.favorites,
-            tooltip: context.lang.favorites,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.settings_outlined),
-            selectedIcon: const Icon(Icons.settings_rounded),
-            label: context.lang.settings,
-            tooltip: context.lang.settings,
-          ),
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        // make when click back back to the last open tab unitl the main and then exit ...
+        if (_tabsHistory.isNotEmpty) {
+          onDestinationSelected(_tabsHistory.removeLast(), false);
+        }
+        return _tabsHistory.isEmpty;
+      },
+      child: Scaffold(
+        body: BlocBuilder<QuranPlayerCubit, QuranPlayerState>(
+          builder: (context, state) {
+            return Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: state.miniPlayer ? 70 : 0),
+                  child: navigationShell,
+                ),
+                if (state.miniPlayer) const MiniPlayerWidget(),
+              ],
+            );
+          },
+        ),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: onDestinationSelected,
+          selectedIndex: navigationShell.currentIndex,
+          destinations: [
+            NavigationDestination(
+              icon: const Icon(Icons.home_outlined),
+              selectedIcon: const Icon(Icons.home_filled),
+              label: context.lang.home,
+              tooltip: context.lang.home,
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.favorite_border_rounded),
+              selectedIcon: const Icon(Icons.favorite),
+              label: context.lang.favorites,
+              tooltip: context.lang.favorites,
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.settings_outlined),
+              selectedIcon: const Icon(Icons.settings_rounded),
+              label: context.lang.settings,
+              tooltip: context.lang.settings,
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  void onDestinationSelected(index) {
+  void onDestinationSelected(index, [bool add = true]) {
     MiniPlayerWidget.controller.animateToHeight(state: PanelState.MIN);
+    if (add && index != navigationShell.currentIndex) _tabsHistory.add(navigationShell.currentIndex);
     navigationShell.goBranch(
       index,
       initialLocation: index == navigationShell.currentIndex,
